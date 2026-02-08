22a374586bfa31c370212e3055fce1817698d764
roz <miroslau@proton.me>
2026-02-08 13:02:15 +0100

    added ex03/ft_print_numbers.c

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+                                                                                                                                                                                    
+void    ft_is_negative(int n);                                                                                                                                                      
+                                                                                                                                                                                    
+void    ft_putchar(char c)                                                                                                                                                          
+{                                                                                                                                                                                   
+        write(1, &c, 1);                                                                                                                                                            
+}                                                                                                                                                                                   
+                                                                                                                                                                                    
+void    ft_is_negative(int n)                                                                                                                                                       
+{                                                                                                                                                                                   
+        if (n >= 0)                                                                                                                                                                 
+        {                                                                                                                                                                           
+                ft_putchar('P');                                                                                                                                                    
+        }                                                                                                                                                                           
+        else                                                                                                                                                                        
+        {                                                                                                                                                                           
+                ft_putchar('N');                                                                                                                                                    
+        }                                                                                                                                                                           
+        ft_putchar('\n');                                                                                                                                                           
+}                                                                                                                                                                                   
+                                                                                                                                                                                    
+int     main(void)                                                                                                                                                                  
+{                                                                                                                                                                                   
+        ft_is_negative(1);                                                                                                                                                          
+}
